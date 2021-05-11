fc3b9ae1cd7312933a487ea33b3bbf5494b243a9
root <>
2021-05-11 05:20:18 +0000

    XC Auto commit: 2021-05-11 05:20:18.982965169 +0000 UTC m=+3070.492371203

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
     http.ListenAndServe(":8080", nil)
 }
 
-// HelloServer is a simple 
+// HelloServer 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
